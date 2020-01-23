51227819db9ffda9c80f3b11ec4951893e833fa6
Ifere Okibe <jioblck@gmail.com>
2020-01-23 10:51:56 +0100

    Updated m_classes.py with class and static method examples

--- m_classes.py
+++ m_classes.py
@@ -32,7 +32,7 @@ print(p1.price)
 
 class Courses:
 
-    #Static variable
+    #Static variable. This variables can be from an Instance of the class
     feeling = "Happily married in a bit"
 
     #Parameterized Constructor
@@ -73,3 +73,36 @@ print(Courses.feeling)
 #Accessing Inner classes
 p2Author=p2.Author("Enobong",27)
 p3Author=p3.Author("Ifere",31)
+
+"""
+Advanced Topics: class and static methods. 
+Class methods are called by a class which is passed to the cls parameter of the method and not by an
+instance of a class which is then passed to the self parameter of the method as earlier methods we have
+seen do. Class methods are marked with a @classmethod decorator.
+Static methods are similar to class methods except they don't receive any additional arguments, they are
+identical to normal functions that belong to a class. Static method are marked with the @staticmethod
+decorator
+"""
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width=width
+        self.height=height
+
+    def calc_area(self):
+        return self.width*self.height
+
+    #Declaring a class method: The method is called on the class instead of an instance of the class
+    #and it return a new object of the class cls
+    @classmethod
+    def square(cls,length):
+        return cls(length,length)
+
+    #Declaring a static method call.
+    @staticmethod
+    def display():
+        print("The is a Static method call")
+
+square1 = Rectangle.square(10)
+print(square1.calc_area())
+Rectangle.display()
